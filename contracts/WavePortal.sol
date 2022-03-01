18ef9ec061f97b1f55a2cf1d0bd1617800e809d4
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-01 16:39:10 +0100

    Update functions to allow waving

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint16) wavesCount;
+
     constructor() {
-        console.log("Hello there, this has been deployed properly!");
+        console.log("Smart contract deployed succesfuylly");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        wavesCount[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+        console.log("being his wave number");
+        console.log(wavesCount[msg.sender]);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
