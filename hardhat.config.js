a6fcfe7f0e0c7cce4da99697ef81abd88db67192
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-03 18:03:17 +0100

    improving solidity to rnadomly pick a winner and give him eth

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,6 +7,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/EocMfErxJ6E07osEnzDBlEM32qElnjFV",
+      accounts: ["00dc0b93963f0c8ad9819ff3fc68d1da894e75cf232a3dcf173bf5efbbecad4a"]
+    },
+  },
 };
